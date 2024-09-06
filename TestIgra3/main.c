573b33b9db577e0d80da7ea3571d3da6a3432cd8
vasilije06 <cvecje@gmail.com>
2024-09-06 21:13:01 +0200

    dodavanje funkcije pedale igrača

--- TestIgra3/main.c
+++ TestIgra3/main.c
@@ -1,38 +1,76 @@
 #include <raylib.h>
 #include <stdio.h>
 
-void ScreenDraw(struct Rectangle box_A);
+void ScreenDraw(struct Rectangle box_A, struct Rectangle box_B, struct Rectangle box_C);
+//void PlayAudio();
+
 
 int main()
 {
 
-	const int screenH = 1200;
-	const int screenW = 1000;
+	const int screenW = 1200;
+	const int screenH = 1000;
+	int brzinaKretanja = 12;
 
-	InitWindow(screenH, screenW, "test igra");
+	InitWindow(screenW, screenH, "test igra");
 
-	Rectangle boxA = { 100, 100, 10, 50 };
-	printf("%d", GetScreenWidth());
+	SetTargetFPS(60);
+
+	Rectangle boxA = { 40.0f , GetScreenWidth() / 2 - 200 , 20, 120};
+	Rectangle boxB = { screenW - 60, GetScreenWidth() / 2.0f - 200 , 20, 120};
+	Rectangle boxC = { screenW / 2 , screenH / 2 - 40, 20, 20};
+
+
 
 	while (!WindowShouldClose())
 	{
 		
-			ScreenDraw(boxA);
+
+		
+
+		if (IsKeyDown(KEY_UP))
+		{
+
+			boxA.y = boxA.y - brzinaKretanja;
+
+		}
+		if (IsKeyDown(KEY_DOWN))
+		{
+
+			boxA.y = boxA.y + brzinaKretanja;
+
+		}
+
+		if (boxA.y <= 0)
+			boxA.y = 0;
+
+		if (boxA.y + boxA.height >= GetScreenHeight())
+			boxA.y = GetScreenHeight() - boxA.height;
+
+
+
+		printf("X:%.lf Y:%.lf\n", boxA.x, boxA.y);
+		ScreenDraw(boxA, boxB, boxC);
 		
 	}
 
 	CloseWindow();
+
 	return 0;
 
 }
 
-void ScreenDraw(struct Rectangle box_A)
+
+void ScreenDraw(struct Rectangle box_A, struct Rectangle box_B, struct Rectangle box_C)
 {
 	ClearBackground(BLACK);
 
 	BeginDrawing();
 
 	DrawRectangleRec(box_A, WHITE);
+	DrawRectangleRec(box_B, WHITE);
+	DrawRectangleRec(box_C, WHITE);
+
 
 	EndDrawing();
 			
